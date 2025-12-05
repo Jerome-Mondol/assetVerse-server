c1aeecf4e0a9f10cce39f1afcd4992c692098651
Jerome Mondol <jeromemondol11@gmail.com>
2025-12-06 02:14:59 +0600

    added: request accept/reject route done

--- src/routes/requests.routes.js
+++ src/routes/requests.routes.js
@@ -8,8 +8,8 @@ const router = express.Router();
 // Create Request 
 router.post('/asset/:id', verifyJWT, verifyRole('employee'), async (req, res) => {
     const { note } = req.body;
-    
-    
+
+
     try {
         const db = getDB();
         const requestsCollection = db.collection('requests');
@@ -18,7 +18,7 @@ router.post('/asset/:id', verifyJWT, verifyRole('employee'), async (req, res) =>
         const { id } = req.params;
         const userID = req.user.id;
 
-        const requestSentBy = await usersCollection.findOne({ _id: new ObjectId(userID)})
+        const requestSentBy = await usersCollection.findOne({ _id: new ObjectId(userID) })
         const asset = await assetsCollection.findOne({ _id: new ObjectId(id) })
         console.log(asset);
         console.log(requestSentBy);
@@ -41,14 +41,67 @@ router.post('/asset/:id', verifyJWT, verifyRole('employee'), async (req, res) =>
         const result = requestsCollection.insertOne(newRequest);
         res.status(201).json({ message: "Request sent successfully" });
     }
-    catch(err) {
+    catch (err) {
         console.log(err);
         res.status(500).json({ message: "Internal Server error" });
     }
 })
 
 // Hr accept request 
+router.patch('/:id/accept', verifyJWT, verifyRole('hr'), async (req, res) => {
+    const { id } = req.params;
 
+    try {
+        const db = getDB();
+        const requestsCollection = db.collection('requests');
+
+        const result = await requestsCollection.updateOne({ _id: new ObjectId(id) },
+            {
+                $set: {
+                    approvalDate: new Date(),
+                    requestStatus: 'approved',
+                    processedBy: req.user.email
+                }
+            })
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ message: "Request not found" });
+        }
+
+        res.status(200).json({ message: "Request accepted" })
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "internal server error" })
+    }
+
+})
+
+// Hr reject request
+router.patch('/:id/reject', verifyJWT, verifyRole('hr'), async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const db = getDB();
+        const requestsCollection = db.collection('requests');
+
+        const result = await requestsCollection.updateOne({ _id: new ObjectId(id) },
+            {
+                $set: {
+                    requestStatus: 'rejected',
+                    processedBy: req.user.email
+                }
+            })
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ message: "Request not found" });
+        }
+
+        res.status(200).json({ message: "Request rejected" })
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "internal server error" })
+    }
+})
 
 
 export default router;
