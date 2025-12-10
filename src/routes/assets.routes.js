c46986644c9ca41f91afb9c70f2935936c873e0c
Jerome Mondol <jeromemondol11@gmail.com>
2025-12-10 15:56:32 +0600

    added: employee asset route added

--- src/routes/assets.routes.js
+++ src/routes/assets.routes.js
@@ -7,8 +7,8 @@ const router = Router();
 
 // create an asset
 router.post('/create', verifyJWT, verifyRole('hr'), async (req, res) => {
-    const { productName, productImage, productType, productQuantity, availableQuantity } = req.body;
-    if (!productName || !productImage || !productType || !productQuantity || !availableQuantity) {
+    const { productName, productImage, productType, productQuantity} = req.body;
+    if (!productName || !productImage || !productType || !productQuantity) {
         return res.status(401).json({ message: "Missing data" });
     }
 
@@ -25,7 +25,7 @@ router.post('/create', verifyJWT, verifyRole('hr'), async (req, res) => {
             productImage,
             productType,
             productQuantity,
-            availableQuantity,
+            availableQuantity: productQuantity,
             dateAdded: new Date(),
             hrEmail: email,
             companyName: hrDetails.companyName,
@@ -65,6 +65,7 @@ router.get('/asset', verifyJWT, verifyRole('employee', 'hr'), async (req, res) =
     }
 });
 
+
 // get assets of a hr 
 router.get('/hr', verifyJWT, verifyRole('hr'), async (req, res) => {
     const { email } = req.query;
@@ -76,12 +77,33 @@ router.get('/hr', verifyJWT, verifyRole('hr'), async (req, res) => {
         const hrAssets = await assetsCollection.find({ hrEmail: email }).toArray();
 
         res.status(200).json(hrAssets);
+        console.log(hrAssets)
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Internal server error" });
     }
 });
 
+
+// get assets of a employee
+router.get('/employee', verifyJWT, verifyRole('employee'), async (req, res) => {
+    const { email } = req.query;
+
+    try {
+        const db = getDB();
+        const assignedAssets = db.collection('assignedAssets');
+        const userAsset = await assignedAssets.find({ employeeEmail: email }).toArray();
+
+        if(userAsset) res.status(200).json(userAsset);
+    }
+    catch(err) {
+        console.log(err)
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+
+// delete a asset
 router.delete('/delete', verifyJWT, verifyRole('hr'), async (req, res) => {
     const { id } = req.query;
     
@@ -106,6 +128,8 @@ router.delete('/delete', verifyJWT, verifyRole('hr'), async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 })
+
+
 
 
 
