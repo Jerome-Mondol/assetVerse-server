0bef4d6956b9691d4016af6ce18298803dc43177
Jerome Mondol <jeromemondol11@gmail.com>
2025-12-11 18:14:29 +0600

    added: remove affiliation route added

--- src/routes/affiliations.routes.js
+++ src/routes/affiliations.routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { verifyJWT, verifyRole } from '../middlewares/auth.js';
 import { getDB } from '../config/db.js';
+import { ObjectId } from 'mongodb';
 const router = express.Router();
 
 router.get('/affiliation', verifyJWT, verifyRole('hr'), async (req, res) => {
@@ -14,6 +15,45 @@ router.get('/affiliation', verifyJWT, verifyRole('hr'), async (req, res) => {
     }
     catch(err) {
         console.log(err);
+        res.status(500).json({ message: "internal server error" });
+    }
+})
+
+router.patch('/remove', verifyJWT, verifyRole('hr'), async(req, res) => {
+    const { id } = req.query;
+    try {
+        const db = getDB();
+        const { email } = req.user;
+        const affiliationsCollection = db.collection('affiliations');
+        const hrCollection = db.collection('hrs');
+        const currentEmployeesOfHr = await hrCollection.findOne({ email });
+
+        
+
+        await affiliationsCollection.updateOne(
+            { _id: new ObjectId(id) },
+            {
+                $set: {
+                    status: 'inactive',
+                }
+            }
+        )
+
+        if(currentEmployeesOfHr) {
+            await hrCollection.updateOne(
+                { email: email },
+                {
+                    $set: {
+                        currentEmployees: currentEmployeesOfHr.currentEmployees - 1
+                    }
+                }
+            )
+        }
+
+        res.status(200).json({ message: "All ok" })
+    }
+    catch(err) {
+        console.log(err)
         res.status(500).json({ message: "internal server error" });
     }
 })
